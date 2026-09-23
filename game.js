8b9075e02a31ddb0e887b84dc6c6f1c9b6290c4b
SuklaasukkulaYT <suklaasukkulayt@gmail.com>
2026-09-23 19:29:01 +0300

    added javascript

---
+++ game.js
@@ -0,0 +1,21 @@
+const CATEGORIES = [
+    {id:"maantieto", name:"Maantieto"},
+    {id:"yleistieto", name:"Yleistieto"},
+    {id:"historia", name:"Historia"},
+    {id:"kulttuuri", name:"Kulttuuri"},
+    {id:"tiede", name:"Tiede"},
+    {id:"urheilu", name:"Urheilu"}
+]
+
+const BOARD_SIZE = 18;
+const boardElement = document.querySelector("#board");
+
+function createBoardCoordinates(){
+    const coordinates = [];
+
+    return coordinates;
+}
+
+function createBoard(){
+    const coordinates = createBoardCoordinates();
+}
